2cab5ea67a99afe588b20f12b2543862da66af33
niczem <ahab@systemli.org>
2023-08-26 16:54:37 +0200

    basic mt functionally added

--- src/classes/sources/Source.ts
+++ src/classes/sources/Source.ts
@@ -1,7 +1,33 @@
 import fetch from 'node-fetch'
+import puppeteer from "puppeteer";
 
 
 class Source{
+    browser = false
+    getBrowser = async function (){
+        if(this.browser){
+            return this.browser;
+        }
+        this.browser = await puppeteer.launch({
+            headless: true,
+            defaultViewport: {
+                width: 1280, // Width of a MacBook screen
+                height: 1400, // Height of a MacBook screen
+            },
+            waitUntil: "domcontentloaded",
+            args: [`--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36`]
+          });
+        return this.browser
+    }
+    
+    convertRawCoordinatesIntoDecimal(input) {
+        let grade = parseInt(input.substring(0, input.indexOf("°")));
+        let rest = input.substring(input.indexOf("°") + 1);
+        let minutes = parseInt(rest.substring(0, rest.indexOf("'")));
+        let seconds = parseInt(rest.substring(rest.indexOf("'") + 1).split('"')[0]);
+        return grade + (minutes + seconds / 60) / 60;
+    }
+
     fetch = async function(url:String,headers:Object,method:String){
         try {
             const response = await fetch(url, {
--- src/classes/sources/ais/mt.ts
+++ src/classes/sources/ais/mt.ts
@@ -2,31 +2,110 @@ import Source from "../Source";
 class Marinetraffic extends Source{
     parseLocation = async function(result:object){
         let location = {
-            timestamp: new Date(result.values.last_pos).toISOString(),
-            latitude: result.values.ship_lat,
-            longitude:result.values.ship_lon,
-            course: result.values.course,
-            speed: result.values.speed,
+            timestamp: result.timestamp,
+            latitude: result.latitude,
+            longitude: result.longitude,
+            course: result.course,
+            speed: result.speed,
             source: 'Marinetraffic',
-            source_type: 'AIS',
-            raw_data: result.values
+            source_type: 'AIS'
         }
         return location;
     }
     getLocation = async (mmsi:Number) =>{
-        let result = await this.fetch("https://www.marinetraffic.com/de/ais/get_info_window_json?asset_type=ship&id=371681",
-        {
-            "accept": "application/json, text/plain, */*",
-            "sec-ch-ua": "\"Not/A)Brand\";v=\"99\", \"Brave\";v=\"115\", \"Chromium\";v=\"115\"",
-            "sec-ch-ua-mobile": "?0",
-            "sec-ch-ua-platform": "\"macOS\"",
-            "vessel-image": "004c895bc2c866751982799a1da126bafb27",
-            "x-requested-with": "XMLHttpRequest",
-            "Referer": "https://www.marinetraffic.com/de/ais/home/centerx:-51.7/centery:45.5/zoom:10",
-            "Referrer-Policy": "strict-origin-when-cross-origin"
-        },
-        "GET"
-        )
+
+
+
+        async function autoScroll(page) {
+            await page.evaluate(async () => {
+              await new Promise((resolve, reject) => {
+                let totalHeight = 0;
+                const distance = 100;
+                const timer = setInterval(() => {
+                  const scrollHeight = document.body.scrollHeight;
+                  window.scrollBy(0, distance);
+                  totalHeight += distance;
+          
+                  if (totalHeight >= scrollHeight) {
+                    clearInterval(timer);
+                    resolve();
+                  }
+                }, 100);
+              });
+            });
+          }
+
+
+        let browser = await this.getBrowser();
+        const page = await browser.newPage();
+        
+        const url = 'https://www.marinetraffic.com/en/ais/details/ships/mmsi:310627000';
+        await page.goto(url);
+      
+        await page.waitForSelector('#vesselDetails_summarySection');
+      
+        // Scroll to the bottom of the page
+        await autoScroll(page);
+      
+          const textContent_course = await page.$eval('#vesselDetails_latestPositionSection', div => div.textContent);
+      
+          const courseRegex = /Speed\/Course: (\d+\.\d+) kn \/ (\d+) °/;
+          const course_match = textContent_course.match(courseRegex);
+          let course,timestamp,latitude,longitude,speed;
+      
+          if (course_match) {
+            course = parseFloat(course_match[2]);
+          } else {
+              throw new Error("could not parse course from marinetraffic");
+          }
+          console.log("course",course);
+      
+          const timestampRegex = /Position Received: (\d{4}-\d{2}-\d{2} \d{2}:\d{2} \D{2}  \D{3})/;
+          const match_time = textContent_course.match(timestampRegex);
+          if (match_time) {
+              timestamp = match_time[1].replace("LT","");
+              console.log(timestamp)
+          } else {
+              throw  new Error("Could not parse timestamp from marinetraffic");
+          }
+      
+          const textContent_pos = await page.$eval('#vesselDetails_summarySection', div => div.textContent);
+            
+            
+          const regex_pos = /position (\d+° \d+' \d+\.\d+" [NS]), (\d+° \d+' \d+\.\d+" [EW])/;
+          const matches = textContent_pos.match(regex_pos);
+          if (matches && matches.length === 3) {
+              latitude = matches[1];
+              longitude = matches[2];
+      
+              console.log("Latitude:", latitude);
+              console.log("Longitude:", longitude);
+          } else {    
+              throw  new Error("could not parse location from marinetraffic")
+          }
+        
+          const speedRegex = /currently sailing at ([\d.]+) knots/;
+          const speedMatch = textContent_pos.match(speedRegex);
+          
+          if (speedMatch && speedMatch.length === 2) {
+            speed = parseFloat(speedMatch[1]);
+          
+            console.log("Speed:", speed, "knots");
+          } else {
+              throw  new Error("Speed not found in the text.");
+          }
+      
+      
+        await browser.close();
+        const result = {
+            course:parseFloat(course),
+            speed:parseFloat(speed),
+            latitude:this.convertRawCoordinatesIntoDecimal(latitude),
+            longitude:this.convertRawCoordinatesIntoDecimal(longitude),
+            timestamp:new Date(timestamp).toISOString()
+
+        }
+        console.log(result);
         return this.parseLocation(result);
     }
 }
