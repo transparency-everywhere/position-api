be18609110807bbb76d425b7e9f29e9c9c22a70f
niczem <niczem@users.noreply.github.com>
2023-12-08 00:22:04 +0000

    Apply linting and formatting changes

--- src/legacy/api.ts
+++ src/legacy/api.ts
@@ -1,101 +1,101 @@
-import Marinetraffic from '../classes/sources/ais/mt'
-import MyShipTracking from '../classes/sources/ais/mst'
+import Marinetraffic from "../classes/sources/ais/mt";
+import MyShipTracking from "../classes/sources/ais/mst";
 
-const moment = require('moment')
-const request = require('request')
+const moment = require("moment");
+const request = require("request");
 
 const debug = (...args) => {
   if (true) {
-    console.log.apply(console, args)
+    console.log.apply(console, args);
   }
+};
+
+async function getLocationFromVF(mmsi, cb) {
+  await getLocationFromMST(mmsi, cb);
 }
 
-async function getLocationFromVF (mmsi, cb) {
-  await getLocationFromMST(mmsi, cb)
-}
-
-async function getLocationFromMST (mmsi, cb) {
-  const mst = new MyShipTracking()
+async function getLocationFromMST(mmsi, cb) {
+  const mst = new MyShipTracking();
   try {
-    const location = await mst.getLocation(mmsi)
-    console.log(location)
+    const location = await mst.getLocation(mmsi);
+    console.log(location);
     cb({
       error: null,
-      data: location
-    })
+      data: location,
+    });
   } catch (e) {
     cb({
       error: e,
-      data: null
-    })
+      data: null,
+    });
   }
 }
 
-async function getLocationFromMT (mmsi, cb) {
-  const mt = new Marinetraffic()
+async function getLocationFromMT(mmsi, cb) {
+  const mt = new Marinetraffic();
 
   try {
-    const location = await mt.getLocation(mmsi)
-    console.log(location)
+    const location = await mt.getLocation(mmsi);
+    console.log(location);
     cb({
       error: null,
-      data: location
-    })
+      data: location,
+    });
   } catch (e) {
     cb({
       error: e,
-      data: null
-    })
+      data: null,
+    });
   }
 }
 
-function getLocation (mmsi, cb) {
-  debug('getting location for vessel: ', mmsi)
+function getLocation(mmsi, cb) {
+  debug("getting location for vessel: ", mmsi);
   getLocationFromVF(mmsi, function (VFResult) {
-    debug('got location from vf', VFResult)
+    debug("got location from vf", VFResult);
 
     getLocationFromMT(mmsi, function (MTResult) {
       if (MTResult.error) {
-        cb(VFResult)
+        cb(VFResult);
       } else {
-        debug('got location from mt', MTResult)
+        debug("got location from mt", MTResult);
         if (!VFResult.data) {
-          return cb(MTResult)
+          return cb(MTResult);
         }
-        const vfDate = moment(VFResult.data.timestamp)
-        const mtDate = moment(MTResult.data.timestamp)
-        const secondsDiff = mtDate.diff(vfDate, 'seconds')
-        debug('time diff in seconds: ', secondsDiff)
+        const vfDate = moment(VFResult.data.timestamp);
+        const mtDate = moment(MTResult.data.timestamp);
+        const secondsDiff = mtDate.diff(vfDate, "seconds");
+        debug("time diff in seconds: ", secondsDiff);
 
-        cb(secondsDiff > 0 ? MTResult : VFResult)
+        cb(secondsDiff > 0 ? MTResult : VFResult);
       }
-    })
-  })
+    });
+  });
 }
 
-function getVesselsInPort (shipPort, cb) {
-  const url = `https://www.marinetraffic.com/en/reports?asset_type=vessels&columns=flag,shipname,photo,recognized_next_port,reported_eta,reported_destination,current_port,imo,ship_type,show_on_live_map,time_of_latest_position,lat_of_latest_position,lon_of_latest_position,current_port_country,notes&current_port_in_name=${shipPort}`
-  debug('getVesselsInPort', url)
+function getVesselsInPort(shipPort, cb) {
+  const url = `https://www.marinetraffic.com/en/reports?asset_type=vessels&columns=flag,shipname,photo,recognized_next_port,reported_eta,reported_destination,current_port,imo,ship_type,show_on_live_map,time_of_latest_position,lat_of_latest_position,lon_of_latest_position,current_port_country,notes&current_port_in_name=${shipPort}`;
+  debug("getVesselsInPort", url);
 
   const headers = {
-    accept: '*/*',
-    'Accept-Language': 'en-US,en;q=0.5',
-    'Accept-Encoding': 'gzip, deflate, brotli',
-    'Vessel-Image': '0053e92efe9e7772299d24de2d0985adea14',
-    'X-Requested-With': 'XMLHttpRequest'
-  }
+    accept: "*/*",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate, brotli",
+    "Vessel-Image": "0053e92efe9e7772299d24de2d0985adea14",
+    "X-Requested-With": "XMLHttpRequest",
+  };
   const options = {
     url,
     headers,
     json: true,
     gzip: true,
     deflate: true,
-    brotli: true
-  }
+    brotli: true,
+  };
   request(options, function (error, response, html) {
     if (
       (!error && response.statusCode === 200) ||
-      (typeof response !== 'undefined' && response.statusCode === 403)
+      (typeof response !== "undefined" && response.statusCode === 403)
     ) {
       return cb(
         response.body.data.map((vessel) => ({
@@ -115,21 +115,21 @@ function getVesselsInPort (shipPort, cb) {
           port_current_id: vessel.PORT_ID,
           port_current: vessel.CURRENT_PORT,
           port_next_id: vessel.NEXT_PORT_ID,
-          port_next: vessel.NEXT_PORT_NAME
-        }))
-      )
+          port_next: vessel.NEXT_PORT_NAME,
+        })),
+      );
     } else {
-      debug('error in getVesselsInPort')
-      cb({ error: 'an unknown error occured' })
-      return false
+      debug("error in getVesselsInPort");
+      cb({ error: "an unknown error occured" });
+      return false;
     }
-  })
+  });
 }
 
 export class api {
-  static getLocationFromVF = getLocationFromVF
-  static getLocationFromMT = getLocationFromMT
-  static getLocationFromMST = getLocationFromMST
-  static getLocation = getLocation
-  static getVesselsInPort = getVesselsInPort
+  static getLocationFromVF = getLocationFromVF;
+  static getLocationFromMT = getLocationFromMT;
+  static getLocationFromMST = getLocationFromMST;
+  static getLocation = getLocation;
+  static getVesselsInPort = getVesselsInPort;
 }
