88040cf1bc0b55ba2f56a7da7b9e3486773b78fc
niczem <ahab@systemli.org>
2024-01-07 12:09:27 +0100

    npm run prettier

--- src/classes/sources/ais/mst.ts
+++ src/classes/sources/ais/mst.ts
@@ -21,7 +21,7 @@ class MyShipTracking extends Source {
 
   getLocation = async (mmsi: number) => {
     const result = await this.fetch(
-      "https://www.myshiptracking.com/vessels/mmsi-"+mmsi,
+      "https://www.myshiptracking.com/vessels/mmsi-" + mmsi,
       {
         accept:
           "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
