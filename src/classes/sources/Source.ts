c8f0bffab621746aa35182ea627d658e1748fed6
niczem <ahab@systemli.org>
2024-01-07 12:12:23 +0100

    npm run linter:fix

--- src/classes/sources/Source.ts
+++ src/classes/sources/Source.ts
@@ -1,50 +1,49 @@
-import fetch from "node-fetch";
-import puppeteer from "puppeteer";
+import fetch from 'node-fetch'
+import puppeteer from 'puppeteer'
 
 class Source {
-  browser: any;
-  constructor() {
-    this.browser = false;
+  browser: any
+  constructor () {
+    this.browser = false
   }
 
   getBrowser: any = async () => {
     if (this.browser) {
-      return this.browser;
+      return this.browser
     }
     this.browser = await puppeteer.launch({
-      headless: "new",
+      headless: true,
       defaultViewport: {
         width: 1280, // Width of a MacBook screen
-        height: 1400, // Height of a MacBook screen
+        height: 1400 // Height of a MacBook screen
       },
-
       // waitUntil: "domcontentloaded",
       args: [
-        "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",
-      ],
-    });
-    return this.browser;
-  };
+        '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36'
+      ]
+    })
+    return this.browser
+  }
 
-  convertRawCoordinatesIntoDecimal(input): number {
-    const grade = parseInt(input.substring(0, input.indexOf("°")));
-    const rest = input.substring(input.indexOf("°") + 1);
-    const minutes = parseInt(rest.substring(0, rest.indexOf("'")));
+  convertRawCoordinatesIntoDecimal (input): number {
+    const grade = parseInt(input.substring(0, input.indexOf('°')))
+    const rest = input.substring(input.indexOf('°') + 1)
+    const minutes = parseInt(rest.substring(0, rest.indexOf("'")))
     const seconds = parseInt(
-      rest.substring(rest.indexOf("'") + 1).split('"')[0],
-    );
-    return grade + (minutes + seconds / 60) / 60;
+      rest.substring(rest.indexOf("'") + 1).split('"')[0]
+    )
+    return grade + (minutes + seconds / 60) / 60
   }
 
   fetch = async function (url: string, headers: any, method: string) {
     const response = await fetch(url, {
       headers,
       body: undefined,
-      method,
-    });
-    const text = await response.text();
-    return text;
-  };
+      method
+    })
+    const text = await response.text()
+    return text
+  }
 }
 
-export default Source;
+export default Source
