1b11af9edaa462c1c5bbb7da3743bb5925181d43
TransFemZboomer <ambre.kihal@protonmail.com>
2024-03-15 23:01:42 +0100

    chore: ran eslint fix

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env ts-node
-import dotenv from "dotenv";
-import Server from "./classes/server";
+import dotenv from 'dotenv'
+import Server from './classes/server'
 
-dotenv.config();
+dotenv.config()
 
-const port = process.env.PORT ?? 5000;
+const port = process.env.PORT ?? 5000
 // eslint-disable-next-line no-new
-new Server(port);
+new Server(port)
