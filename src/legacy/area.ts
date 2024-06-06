c39036acb7ddd714f14cadf7579cf574724130df
niczem <ahab@systemli.org>
2024-06-06 06:36:57 +0200

    npm run lint:fix

--- src/legacy/area.ts
+++ src/legacy/area.ts
@@ -1,19 +1,19 @@
-const scraper = require("./lib/puppeteer");
+const scraper = require('./lib/puppeteer')
 
 const fetchResultByArea = async (area, time, cb) => {
   await scraper.fetch(
     {
       url: `https://www.marinetraffic.com/en/reports?asset_type=vessels&columns=time_of_latest_position:desc,flag,shipname,photo,recognized_next_port,reported_eta,reported_destination,current_port,imo,ship_type,show_on_live_map,area,lat_of_latest_position,lon_of_latest_position&area_in=${area}&time_of_latest_position_between=${time}`,
       referer:
-        "https://www.marinetraffic.com/en/data/?asset_type=vessels&columns=time_of_latest_position:desc,flag,shipname,photo,recognized_next_port,reported_eta,reported_destination,current_port,imo,ship_type,show_on_live_map,area,lat_of_latest_position,lon_of_latest_position&area_in|in|West%20Mediterranean,East%20Mediterranean|area_in=WMED,EMED&time_of_latest_position_between|gte|time_of_latest_position_between=60,525600",
-      responseSelector: "/en/reports?",
+        'https://www.marinetraffic.com/en/data/?asset_type=vessels&columns=time_of_latest_position:desc,flag,shipname,photo,recognized_next_port,reported_eta,reported_destination,current_port,imo,ship_type,show_on_live_map,area,lat_of_latest_position,lon_of_latest_position&area_in|in|West%20Mediterranean,East%20Mediterranean|area_in=WMED,EMED&time_of_latest_position_between|gte|time_of_latest_position_between=60,525600',
+      responseSelector: '/en/reports?',
       extraHeaders: {
-        "vessel-image": "0053e92efe9e7772299d24de2d0985adea14",
-      },
+        'vessel-image': '0053e92efe9e7772299d24de2d0985adea14'
+      }
     },
-    cb,
-  );
-};
+    cb
+  )
+}
 
 const mapResult = (result) => {
   return result.data.map((vessel) => ({
@@ -33,51 +33,51 @@ const mapResult = (result) => {
     port_current_id: vessel.PORT_ID,
     port_current: vessel.CURRENT_PORT,
     port_next_id: vessel.NEXT_PORT_ID,
-    port_next: vessel.NEXT_PORT_NAME,
-  }));
-};
+    port_next: vessel.NEXT_PORT_NAME
+  }))
+}
 
-const fetchVesselsInArea: Function = (regions = ["WMED", "EMED"], cb) => {
-  const timeframe = [60, 525600];
-  fetchResultByArea(regions.join(","), timeframe.join(","), (result) => {
+const fetchVesselsInArea: Function = (regions = ['WMED', 'EMED'], cb) => {
+  const timeframe = [60, 525600]
+  fetchResultByArea(regions.join(','), timeframe.join(','), (result) => {
     if (!result?.data.length) {
-      return cb(null);
+      return cb(null)
     }
-    return cb(mapResult(result));
-  });
-};
+    return cb(mapResult(result))
+  })
+}
 
 const fetchResultNearMe = async (lat, lng, distance, time, cb) => {
   await scraper.fetch(
     {
       url: `https://www.marinetraffic.com/en/reports?asset_type=vessels&columns=time_of_latest_position:desc,flag,shipname,photo,recognized_next_port,reported_eta,reported_destination,current_port,imo,ship_type,show_on_live_map,area,lat_of_latest_position,lon_of_latest_position&time_of_latest_position_between=${time}&near_me=${lat},${lng},${distance}`,
       referer:
-        "https://www.marinetraffic.com/en/data/?asset_type=vessels&columns=time_of_latest_position:desc,flag,shipname,photo,recognized_next_port,reported_eta,reported_destination,current_port,imo,ship_type,show_on_live_map,area,lat_of_latest_position,lon_of_latest_position&area_in|in|West%20Mediterranean,East%20Mediterranean|area_in=WMED,EMED&time_of_latest_position_between|gte|time_of_latest_position_between=60,525600",
-      responseSelector: "/en/reports?",
+        'https://www.marinetraffic.com/en/data/?asset_type=vessels&columns=time_of_latest_position:desc,flag,shipname,photo,recognized_next_port,reported_eta,reported_destination,current_port,imo,ship_type,show_on_live_map,area,lat_of_latest_position,lon_of_latest_position&area_in|in|West%20Mediterranean,East%20Mediterranean|area_in=WMED,EMED&time_of_latest_position_between|gte|time_of_latest_position_between=60,525600',
+      responseSelector: '/en/reports?',
       extraHeaders: {
-        "vessel-image": "0053e92efe9e7772299d24de2d0985adea14",
-      },
+        'vessel-image': '0053e92efe9e7772299d24de2d0985adea14'
+      }
     },
-    cb,
-  );
-};
+    cb
+  )
+}
 
 const fetchVesselsNearMe: Function = (
   lat = 51.7419,
   lng = 3.89773,
   distance = 2,
-  cb,
+  cb
 ) => {
-  const timeframe = [60, 525600];
-  fetchResultNearMe(lat, lng, distance, timeframe.join(","), (result: any) => {
+  const timeframe = [60, 525600]
+  fetchResultNearMe(lat, lng, distance, timeframe.join(','), (result: any) => {
     if (!result?.data.length) {
-      return cb(null);
+      return cb(null)
     }
-    return cb(mapResult(result));
-  });
-};
+    return cb(mapResult(result))
+  })
+}
 
 export class areaApi {
-  static fetchVesselsInArea = fetchVesselsInArea;
-  static fetchVesselsNearMe = fetchVesselsNearMe;
+  static fetchVesselsInArea = fetchVesselsInArea
+  static fetchVesselsNearMe = fetchVesselsNearMe
 }
