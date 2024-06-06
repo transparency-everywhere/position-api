4caf09844e80d0e37daa19ec348c6c52dc109db7
niczem <niczem@users.noreply.github.com>
2024-06-06 04:37:38 +0000

    Apply linting and formatting changes

--- src/classes/sources/ais/mt.ts
+++ src/classes/sources/ais/mt.ts
@@ -1,4 +1,4 @@
-import Source from '../Source'
+import Source from "../Source";
 class Marinetraffic extends Source {
   parseLocation = async function (result: any) {
     const location = {
@@ -7,43 +7,43 @@ class Marinetraffic extends Source {
       longitude: result.longitude,
       course: result.course,
       speed: result.speed,
-      source: 'Marinetraffic',
-      source_type: 'AIS'
-    }
-    return location
-  }
+      source: "Marinetraffic",
+      source_type: "AIS",
+    };
+    return location;
+  };
 
   getLocation = async (mmsi: number) => {
-    const browser = await this.getBrowser()
-    const page = await browser.newPage()
+    const browser = await this.getBrowser();
+    const page = await browser.newPage();
 
     const url =
-      'https://www.marinetraffic.com/en/ais/details/ships/mmsi:' + mmsi
-    await page.goto(url)
+      "https://www.marinetraffic.com/en/ais/details/ships/mmsi:" + mmsi;
+    await page.goto(url);
     // let parsedData = null;
 
     const waitForResponse = new Promise((resolve) => {
-      page.on('response', async (response) => {
-        const request = response.request()
-        if (request.url().includes('latestPosition')) {
-          const jsonresult = await response.text()
-          const parsedData = JSON.parse(jsonresult)
-          resolve(parsedData)
+      page.on("response", async (response) => {
+        const request = response.request();
+        if (request.url().includes("latestPosition")) {
+          const jsonresult = await response.text();
+          const parsedData = JSON.parse(jsonresult);
+          resolve(parsedData);
         }
-      })
-    })
+      });
+    });
 
-    const parsedData: any = await waitForResponse
-    browser.close()
+    const parsedData: any = await waitForResponse;
+    browser.close();
     const result = {
       course: parseFloat(parsedData.course),
       speed: parseFloat(parsedData.speed),
       latitude: parseFloat(parsedData.lat),
       longitude: parseFloat(parsedData.lon),
-      timestamp: new Date(parsedData.lastPos * 1000).toISOString() // assuming lastPos is in seconds
-    }
-    return await this.parseLocation(result)
-  }
+      timestamp: new Date(parsedData.lastPos * 1000).toISOString(), // assuming lastPos is in seconds
+    };
+    return await this.parseLocation(result);
+  };
 }
 
-export default Marinetraffic
+export default Marinetraffic;
