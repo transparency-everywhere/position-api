8d8ea7816d3c056c802a130a0e3cb079c7c23950
niczem <niczem@users.noreply.github.com>
2025-07-06 10:26:30 +0000

    Apply linting and formatting changes

--- src/classes/sources/adsb/adsbe.ts
+++ src/classes/sources/adsb/adsbe.ts
@@ -1,4 +1,4 @@
-import Source from '../Source'
+import Source from "../Source";
 
 class ADSBExchange extends Source {
   parseLocation = async function (result: any) {
@@ -9,73 +9,75 @@ class ADSBExchange extends Source {
       course: result.course,
       speed: result.speed,
       raw: result.raw,
-      source: 'adsbExchange',
-      source_type: 'adsb'
-    }
-    return location
-  }
+      source: "adsbExchange",
+      source_type: "adsb",
+    };
+    return location;
+  };
 
   getLocation = async (icao: string) => {
     /// <reference lib="dom" />
-    const fetch = (await import('node-fetch')).default
+    const fetch = (await import("node-fetch")).default;
     const response = await fetch(
       `https://globe.adsbexchange.com/data/traces/${icao.slice(
-        -2
+        -2,
       )}/trace_recent_${icao}.json`,
       {
         headers: {
-          accept: 'application/json, text/javascript, */*; q=0.01',
-          'accept-language': 'de-DE,de;q=0.9',
-          'sec-ch-ua':
+          accept: "application/json, text/javascript, */*; q=0.01",
+          "accept-language": "de-DE,de;q=0.9",
+          "sec-ch-ua":
             '"Chromium";v="116", "Not)A;Brand";v="24", "Brave";v="116"',
-          'sec-ch-ua-mobile': '?0',
-          'sec-ch-ua-platform': '"macOS"',
-          'sec-fetch-dest': 'empty',
-          'sec-fetch-mode': 'cors',
-          'sec-fetch-site': 'same-origin',
-          'sec-gpc': '1',
-          'x-requested-with': 'XMLHttpRequest',
-          Referer: 'https://globe.adsbexchange.com/?icao=39e68b',
-          'Referrer-Policy': 'strict-origin-when-cross-origin'
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": '"macOS"',
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-origin",
+          "sec-gpc": "1",
+          "x-requested-with": "XMLHttpRequest",
+          Referer: "https://globe.adsbexchange.com/?icao=39e68b",
+          "Referrer-Policy": "strict-origin-when-cross-origin",
         },
         body: undefined,
-        method: 'GET'
-      }
-    )
+        method: "GET",
+      },
+    );
 
     if (!response.ok) {
       if (response.status === 404) {
-        console.error(`Data not found for ICAO: ${icao}`)
-        return null // Return null or fallback data
+        console.error(`Data not found for ICAO: ${icao}`);
+        return null; // Return null or fallback data
       }
-      throw new Error(`Failed to fetch data: ${response.status} ${response.statusText}`)
+      throw new Error(
+        `Failed to fetch data: ${response.status} ${response.statusText}`,
+      );
     }
 
-    const contentType = response.headers.get('content-type')
-    if (!contentType?.includes('application/json')) {
-      throw new Error(`Unexpected content type: ${contentType}`)
+    const contentType = response.headers.get("content-type");
+    if (!contentType?.includes("application/json")) {
+      throw new Error(`Unexpected content type: ${contentType}`);
     }
 
-    const body = await response.text()
-    let bodyjson
+    const body = await response.text();
+    let bodyjson;
     try {
-      bodyjson = JSON.parse(body)
+      bodyjson = JSON.parse(body);
     } catch (err) {
       if (err instanceof Error) {
-        throw new Error(`Failed to parse JSON: ${err.message}`)
+        throw new Error(`Failed to parse JSON: ${err.message}`);
       } else {
-        throw new Error('Failed to parse JSON: Unknown error')
+        throw new Error("Failed to parse JSON: Unknown error");
       }
     }
 
-    const data = bodyjson as { timestamp: any, trace: any } // Add this line
+    const data = bodyjson as { timestamp: any; trace: any }; // Add this line
 
-    const timestamp = data.timestamp
-    const trace = data.trace
-    const last_trace = trace[trace.length - 1]
+    const timestamp = data.timestamp;
+    const trace = data.trace;
+    const last_trace = trace[trace.length - 1];
     const [time_offset, lat, lon, alt_baro, ground_speed, ground_track] =
-      last_trace
-    const last_trace_timestamp = timestamp + time_offset
+      last_trace;
+    const last_trace_timestamp = timestamp + time_offset;
     const location = this.parseLocation({
       timestamp: last_trace_timestamp,
       latitude: lat,
@@ -83,47 +85,47 @@ class ADSBExchange extends Source {
       course: ground_track,
       speed: ground_speed, // knots
       altitude: alt_baro,
-      raw: bodyjson
-    })
-    return await location
-  }
+      raw: bodyjson,
+    });
+    return await location;
+  };
 
   getLocationFull = async (icao: string) => {
     /// <reference lib="dom" />
-    const fetch = (await import('node-fetch')).default
+    const fetch = (await import("node-fetch")).default;
     const response = await fetch(
       `https://globe.adsbexchange.com/data/traces/${icao.slice(
-        -2
+        -2,
       )}/trace_full_${icao}.json`,
       {
         headers: {
-          accept: 'application/json, text/javascript, */*; q=0.01',
-          'accept-language': 'de-DE,de;q=0.9',
-          'sec-ch-ua':
+          accept: "application/json, text/javascript, */*; q=0.01",
+          "accept-language": "de-DE,de;q=0.9",
+          "sec-ch-ua":
             '"Chromium";v="116", "Not)A;Brand";v="24", "Brave";v="116"',
-          'sec-ch-ua-mobile': '?0',
-          'sec-ch-ua-platform': '"macOS"',
-          'sec-fetch-dest': 'empty',
-          'sec-fetch-mode': 'cors',
-          'sec-fetch-site': 'same-origin',
-          'sec-gpc': '1',
-          'x-requested-with': 'XMLHttpRequest',
-          Referer: 'https://globe.adsbexchange.com/?icao=39e68b',
-          'Referrer-Policy': 'strict-origin-when-cross-origin'
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": '"macOS"',
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-origin",
+          "sec-gpc": "1",
+          "x-requested-with": "XMLHttpRequest",
+          Referer: "https://globe.adsbexchange.com/?icao=39e68b",
+          "Referrer-Policy": "strict-origin-when-cross-origin",
         },
         body: undefined,
-        method: 'GET'
-      }
-    )
-    const body = await response.text()
-    const bodyjson = JSON.parse(body)
-    const data = bodyjson as { timestamp: any, trace: any } // Add this line
+        method: "GET",
+      },
+    );
+    const body = await response.text();
+    const bodyjson = JSON.parse(body);
+    const data = bodyjson as { timestamp: any; trace: any }; // Add this line
 
-    const timestamp = data.timestamp
-    const trace = data.trace
+    const timestamp = data.timestamp;
+    const trace = data.trace;
 
-    console.log(trace)
-    const last_trace = trace[trace.length - 1]
+    console.log(trace);
+    const last_trace = trace[trace.length - 1];
     const [
       time_offset,
       lat,
@@ -133,15 +135,15 @@ class ADSBExchange extends Source {
       unknown2,
       unknown3,
       unknown4,
-      unknown5
-    ] = last_trace
-    console.log('check that lat and lon in correct order!!')
-    console.log('last trace')
-    console.log(last_trace)
-    console.log('=========')
-    const last_trace_timestamp = timestamp + time_offset
-    console.log(last_trace_timestamp)
-    console.log(new Date(last_trace_timestamp * 1000))
+      unknown5,
+    ] = last_trace;
+    console.log("check that lat and lon in correct order!!");
+    console.log("last trace");
+    console.log(last_trace);
+    console.log("=========");
+    const last_trace_timestamp = timestamp + time_offset;
+    console.log(last_trace_timestamp);
+    console.log(new Date(last_trace_timestamp * 1000));
     console.log(
       time_offset,
       lat,
@@ -151,9 +153,9 @@ class ADSBExchange extends Source {
       unknown2,
       unknown3,
       unknown4,
-      unknown5
-    )
-  }
+      unknown5,
+    );
+  };
 }
 
-export default ADSBExchange
+export default ADSBExchange;
